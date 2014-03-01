7bfc76bdf895702bf936b67dc3a7d42354818065
Alex Sansom <alex_sansom@hotmail.com>
2014-03-01 14:39:42 +0000

    Document company search templates

--- templates/company-search-page.tpl.php
+++ templates/company-search-page.tpl.php
@@ -1,14 +1,18 @@
-<?php 
+<?php
 /**
  * @file
  * Template to display company 'search by text' page.
+ *
+ * Available variables:
+ * - $form: For allowing search of companies.
+ *
+ * @ingoup themeable
  */
 ?>
-
-
 <p>
   Page enabling a user search for a company by text.
 </p>
 <div>
   <?php print $form ?>
 </div>
+
--- templates/company-search-result.tpl.php
+++ templates/company-search-result.tpl.php
@@ -2,10 +2,19 @@
 /**
  * @file
  * Template to display company search results.
+ *
+ * Available variables:
+ * - $error: Error message indicating that the gateway search failed.
+ * - $company_list: Array of 'exact' and non-exact company names. Includes:
+ * - $company_list['exact']['name']: Nearest match company name.
+ * - $company_list['exact']['number']: Nearest match company number.
+ * - $company_list['match']: Array of near matches. Each item comprised of:
+ * - $company_list['match']['name']: Company name.
+ * - $company_list['match']['number']: Company number.
+ *
+ * @ingroup themeable
  */
 ?>
-
-
 <?php if ($error): ?>
   <p>
     Error: <?php print $error ?>
@@ -13,24 +22,22 @@
 <?php endif; ?>
 
 <?php if (count($company_list['exact']) > 0): ?>
-<h3>Exact match:</h3>
-<div>
-  <?php print l($company_list['exact']['name'], 'chxmlgw/company-details/' . $company_list['exact']['number'], array('attributes' => array('title' => t('View more details'), 'class' => array('match', 'exact')))); ?>
-</div>
+  <h3>Exact match:</h3>
+  <div>
+    <?php print l($company_list['exact']['name'], 'chxmlgw/company-details/' . $company_list['exact']['number'], array('attributes' => array('title' => t('View more details'), 'class' => array('match', 'exact')))); ?>
+  </div>
 <?php endif; ?>
 
 <h3>Non-exact matches:</h3>
 <div>
-<?php
-  $index = 0; 
-  foreach ($company_list['match'] as $company): 
-    $index++;
-?>
-    <li><?php print l($company['name'], 'chxmlgw/company-details/' . $company['number'], array('attributes' => array('title' => t('View more details'), 'class' => array('match', 'non-exact', 'non-exact-' . $index)))); ?></li>
+  <ul>
+    <?php foreach ($company_list['match'] as $company): ?>
+      <li><?php print l($company['name'], 'chxmlgw/company-details/' . $company['number'], array('attributes' => array('title' => t('View more details'), 'class' => array('match', 'non-exact')))); ?></li>
+    <?php endforeach; ?>
   </ul>
-<?php endforeach; ?>
 </div>
 
 <p>
   <?php print l(t('Search again?'), 'chxmlgw/company-search'); ?>
 </p>
+
